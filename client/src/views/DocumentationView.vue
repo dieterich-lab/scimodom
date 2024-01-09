836f3489b0c6751ca4fbd746d3cf26b8d352c169
Etienne Boileau <boileau@uni-heidelberg.de>
2024-01-09 17:01:50 +0100

    WIP #39

--- client/src/views/DocumentationView.vue
+++ client/src/views/DocumentationView.vue
@@ -8,8 +8,26 @@
           Documentation
         </span>
       </h1>
-      <p class="text-lg font-normal text-gray-500 lg:text-xl">How to...</p>
+      <p class="text-lg font-normal text-gray-500 lg:text-xl">General information and how-to</p>
       <Divider :pt="{ root: { class: 'bg-crmg' } }" />
+      <div class="text-justify m-6">
+        <h1 class="font-ham text-4xl font-semibold m-auto p-4">
+          <span>Introduction</span>
+        </h1>
+        <p class="indent-4 text-lg mt-2 mb-2">
+          Sci-ModoM allows to query RNA modification sites according to RNA type, technology,
+          species, and genomic features (Search).
+        </p>
+        <p class="indent-4 text-lg mt-2 mb-2">
+          Data can be accessed at the study- or dataset-level. Modifications are associated with a
+          given dataset. Each dataset is assigned a EUFID or EUF identifier. One or more dataset can
+          belong to a study or project. Projects are identified using a SMID or Sci-ModoM
+          identifier. A dataset can include one or more modification, but is uniquely associated
+          with a given technology and species (including cell type, tissue, or organ). Hence it is
+          easy to think of a dataset as RNA modifications stored in a given bedRMod file (EU
+          format).
+        </p>
+      </div>
     </SectionLayout>
   </DefaultLayout>
 </template>
