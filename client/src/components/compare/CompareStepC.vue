ed67a42bab97136ac678e115a2848d9aa42eafc2
Etienne Boileau <boileau@uni-heidelberg.de>
2024-05-15 13:59:45 +0200

    ADD step C

---
+++ client/src/components/compare/CompareStepC.vue
@@ -0,0 +1,67 @@
+<script setup>
+const model = defineModel()
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <div class="flex flex-row">
+      <div class="w-1/2">
+        <RadioButton v-model="model" inputId="criteria1" name="step3" value="intersect-true" />
+        <label for="criteria1" class="ml-2">
+          <span class="inline font-bold">Intersection</span>
+        </label>
+        <p class="mt-0 ml-8 text-sm">Search for overlaps between A and B on the same strand.</p>
+      </div>
+      <div class="w-1/2">
+        <RadioButton v-model="model" inputId="criteria2" name="step3" value="intersect-false" />
+        <label for="criteria3" class="ml-2">
+          <span class="inline font-bold">Intersection (strand-unaware)</span>
+        </label>
+        <p class="mt-0 ml-8 text-sm">
+          Search for overlaps between A and B without respect to strand.
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-row">
+      <div class="w-1/2">
+        <RadioButton v-model="model" inputId="criteria3" name="step3" value="closest-true" />
+        <label for="criteria3" class="ml-2">
+          <span class="inline font-bold">Closest</span>
+        </label>
+        <p class="mt-0 ml-8 text-sm">
+          Search for closest non-overlaps in B (wrt. A) on the same strand.
+        </p>
+      </div>
+      <div class="w-1/2">
+        <RadioButton v-model="model" inputId="criteria4" name="step3" value="closest-false" />
+        <label for="criteria3" class="ml-2">
+          <span class="inline font-bold">Closest (strand-unaware)</span>
+        </label>
+        <p class="mt-0 ml-8 text-sm">
+          Search for closest non-overlaps in B (wrt. A) without respect to strand.
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-row">
+      <div class="w-1/2">
+        <RadioButton v-model="model" inputId="criteria5" name="step3" value="subtract-true" />
+        <label for="criteria5" class="ml-2">
+          <span class="inline font-bold">Difference</span>
+        </label>
+        <p class="mt-0 ml-8 text-sm">
+          Search for strict non-overlaps in A (modifications in A but not in B) on the same strand.
+        </p>
+      </div>
+      <div class="w-1/2">
+        <RadioButton v-model="model" inputId="criteria6" name="step3" value="subtract-false" />
+        <label for="criteria6" class="ml-2">
+          <span class="inline font-bold">Difference (strand-unaware)</span>
+        </label>
+        <p class="mt-0 ml-8 text-sm">
+          Search for strict non-overlaps in A (modifications in A but not in B) without respect to
+          strand.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- client/src/views/CompareView.vue
+++ client/src/views/CompareView.vue
@@ -5,6 +5,7 @@ import { HTTP } from '@/services/API.js'
 
 import CompareStepA from '@/components/compare/CompareStepA.vue'
 import CompareStepB from '@/components/compare/CompareStepB.vue'
+import CompareStepC from '@/components/compare/CompareStepC.vue'
 import StyledHeadline from '@/components/ui/StyledHeadline.vue'
 import SubTitle from '@/components/ui/SubTitle.vue'
 
@@ -170,100 +171,9 @@ const buttonPt = {
           </TabPanel>
           <TabPanel header="Select query criteria">
             <div>
-              <form @submit="onSubmit" class="flex flex-col gap-4">
-                <div class="flex flex-row">
-                  <div class="w-1/2">
-                    <RadioButton
-                      v-model="queryCriteria"
-                      inputId="criteria1"
-                      name="step3"
-                      value="intersect-true"
-                    />
-                    <label for="criteria1" class="ml-2">
-                      <span class="inline font-bold">Intersection</span>
-                    </label>
-                    <p class="mt-0 ml-8 text-sm">
-                      Search for overlaps between A and B on the same strand.
-                    </p>
-                  </div>
-                  <div class="w-1/2">
-                    <RadioButton
-                      v-model="queryCriteria"
-                      inputId="criteria2"
-                      name="step3"
-                      value="intersect-false"
-                    />
-                    <label for="criteria3" class="ml-2">
-                      <span class="inline font-bold">Intersection (strand-unaware)</span>
-                    </label>
-                    <p class="mt-0 ml-8 text-sm">
-                      Search for overlaps between A and B without respect to strand.
-                    </p>
-                  </div>
-                </div>
-                <div class="flex flex-row">
-                  <div class="w-1/2">
-                    <RadioButton
-                      v-model="queryCriteria"
-                      inputId="criteria3"
-                      name="step3"
-                      value="closest-true"
-                    />
-                    <label for="criteria3" class="ml-2">
-                      <span class="inline font-bold">Closest</span>
-                    </label>
-                    <p class="mt-0 ml-8 text-sm">
-                      Search for closest non-overlaps in B (wrt. A) on the same strand.
-                    </p>
-                  </div>
-                  <div class="w-1/2">
-                    <RadioButton
-                      v-model="queryCriteria"
-                      inputId="criteria4"
-                      name="step3"
-                      value="closest-false"
-                    />
-                    <label for="criteria3" class="ml-2">
-                      <span class="inline font-bold">Closest (strand-unaware)</span>
-                    </label>
-                    <p class="mt-0 ml-8 text-sm">
-                      Search for closest non-overlaps in B (wrt. A) without respect to strand.
-                    </p>
-                  </div>
-                </div>
-                <div class="flex flex-row">
-                  <div class="w-1/2">
-                    <RadioButton
-                      v-model="queryCriteria"
-                      inputId="criteria5"
-                      name="step3"
-                      value="subtract-true"
-                    />
-                    <label for="criteria5" class="ml-2">
-                      <span class="inline font-bold">Difference</span>
-                    </label>
-                    <p class="mt-0 ml-8 text-sm">
-                      Search for strict non-overlaps in A (modifications in A but not in B) on the
-                      same strand.
-                    </p>
-                  </div>
-                  <div class="w-1/2">
-                    <RadioButton
-                      v-model="queryCriteria"
-                      inputId="criteria6"
-                      name="step3"
-                      value="subtract-false"
-                    />
-                    <label for="criteria6" class="ml-2">
-                      <span class="inline font-bold">Difference (strand-unaware)</span>
-                    </label>
-                    <p class="mt-0 ml-8 text-sm">
-                      Search for strict non-overlaps in A (modifications in A but not in B) without
-                      respect to strand.
-                    </p>
-                  </div>
-                </div>
-                <div>
+              <form @submit="onSubmit">
+                <div class="h-52">
+                  <CompareStepC v-model="queryCriteria" />
                   <Button
                     icon="pi pi-sync"
                     size="small"
@@ -271,6 +181,7 @@ const buttonPt = {
                     label="Submit"
                     :disabled="disabled"
                     :loading="loading"
+                    class="mt-4"
                   />
                   <small id="text-error" class="p-4 select-none text-sm text-red-700">
                     <i
