780284cf4bf86e35d27bfaa18547f1f73ad4ba8b
Etienne Boileau <boileau@uni-heidelberg.de>
2025-05-23 15:41:52 +0200

    FIX allowImportingTsExtensions

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, configDefaults, defineConfig } from 'vitest/config'
-import viteConfig from './vite.config.mts'
+import viteConfig from './vite.config'
 
 export default mergeConfig(
   viteConfig,
